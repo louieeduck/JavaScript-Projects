b76fa9b2707c92b34d14294faff9f754877306aa
Evan <evanfretheim@gmail.com>
2021-07-02 16:37:18 -0500

    JS

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+var A = "Hello"; var blues ="My name is Evan"; // Set variables A and blue
+var blues = blues.fontcolor ("blue");  // turn variable B to blue
+var A = A.fontcolor ("red"); // turn variable A to red
+document.write(blues);  // print variable blue
+document.write(A); // print variable A
+window.alert("Hello");
+var D = "My name is Evan," + "I am 19!";
+document.write(D);
+
+var C = 3+3; // set variable c
+document.write(C); // print C
+
+// Comment //
+
