dd28a440d1e179d9b7c25066dba010c31fe465ed
Evan <evanfretheim@gmail.com>
2021-07-06 14:44:16 -0500

    fixes

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,10 +1,10 @@
-function Dictionary() {
-    var me = {
-        Age: "19",
-        Gender: "Male",
-        Height: "6'",
-        Hair: "Brown",
+function Dictionary() {  // create dictionary function
+    var me = {  // create dictionary variable
+        Age: "19",  // Age key
+        Gender: "Male",  // Gender key
+        Height: "6'",  // height key
+        Hair: "Brown", // hair key
     };
-    delete me.Age;
+    delete me.Age;  // Deleting the age key
     document.getElementById("Dictionary").innerHTML = me.Age;
 }
---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,20 @@
+document.write(typeof 3);
+
+document.write("5" + 5);
+
+function my() {
+    var div = 0/0;
+    document.getElementById("Test").innerHTML = "0/0" + div;
+}
+
+function myfunc() {
+
+document.getElementById("Test1").innerHTML = isNaN('This is a string');
+
+}
+
+function myfunc2() {
+
+document.getElementById("Test2").innerHTML = isNaN('007');
+
+}
