8c5e73c6b368d43634f06273ae5862dd6dfa7cca
Evan <evanfretheim@gmail.com>
2021-07-06 12:58:06 -0500

    functions

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,8 @@
+function My_First_Function () {
+    var sentence = "This is a string" // Create variable //
+    var result = sentence.fontcolor("red");  // Turn variable red //
+    document.getElementById("demo"); 
+    var concatsent = sentence += " Concatenated part of the string.";  // Create concat //
+    document.write(concatsent);  // display concat //
+
+}
