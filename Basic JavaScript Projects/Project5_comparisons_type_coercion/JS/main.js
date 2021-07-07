7925f638d79bd86864f6eb20ba3b03c4110438e7
Evan <evanfretheim@gmail.com>
2021-07-07 12:36:47 -0500

    project5

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -18,3 +18,49 @@ function myfunc2() {
 document.getElementById("Test2").innerHTML = isNaN('007');
 
 }
+
+document.write(3E310);
+document.write(-3E310);
+document.write(10 > 2);
+document.write(10 < 2);
+console.log(2+2);
+console.log(isNaN(5>10));
+document.write(10==10);
+document.write(3==10);
+
+X=5;
+Y=5;
+document.write(X===Y);
+
+X=5;
+Y="10";
+document.write(X===Y);
+
+X=5;
+Y="5";
+document.write(X===Y);
+
+X=5;
+Y=10;
+document.write(X===Y);
+
+X=10
+Y=5
+Z=2
+document.write(X > Y && Y > Z);
+document.write(X > Y && Y < Z);
+
+X=10
+Y=5
+Z=2
+document.write(X > Y || Y > Z);
+document.write(X < Y || Y < Z);
+
+function notfunc() {
+    document.getElementById("Not").innerHTML= !(20 > 10);
+}
+
+function notfunct() {
+    document.getElementById("Nots").innerHTML= !(20 < 10);
+}
+
