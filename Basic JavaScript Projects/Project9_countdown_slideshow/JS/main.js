7f51faa48c1d8564432503cb469b6f6631879ed6
Evan <evanfretheim@gmail.com>
2021-07-10 18:41:22 -0500

    slide

---
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,37 @@
+function countdown() {
+    var seconds = document.getElementById("seconds").value;  // create function
+
+    function tick() {
+        seconds = seconds - 1;
+        timer.innerHTML = seconds;
+        setTimeout(tick, 1000);
+    if(seconds==-1) {
+        alert("Times up!");  // create function where timer goes down every second
+    }
+    }
+    tick();
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);  // create variable
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);  // create variable
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);  // create variable
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementById("mySlides");
+    var dots = document.getElementById("dot");
+    if (n > slides.length) {slideIndex = 1}  
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].className = dot[i].className.replace(" active", "");   // Function to create the movement of the slides
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
