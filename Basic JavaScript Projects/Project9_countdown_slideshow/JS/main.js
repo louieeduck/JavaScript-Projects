6825424f135be07797a503576b544711a859123f
Evan <evanfretheim@gmail.com>
2021-07-11 19:38:20 -0500

    corrections

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -12,7 +12,6 @@ function countdown() {
     tick();
 }
 
-
 var slideIndex = 1;
 showSlides(slideIndex);  // create variable
 
@@ -31,10 +30,11 @@ function showSlides(n) {
     if (n > slides.length) {slideIndex = 1}  
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
-        slides[i].className = dot[i].className.replace(" active", "");   // Function to create the movement of the slides
+        slides[i].style.display = "none";
     }
-    // slides[slideIndex-1].style.display = "block";
-    // dots[slideIndex-1].className += " active";
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");   // Function to create the movement of the slides
+    }
+     slides[slideIndex-1].style.display = "block";
+     dots[slideIndex-1].className += " active";
 }
-
-
